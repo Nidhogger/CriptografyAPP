87a38af1b331f8cf631ef7ca004e24fddf8503f2
Nidhogger <62850635+Nidhogger@users.noreply.github.com>
2022-08-23 22:02:38 -0300

    criação das rotas restantes e controlers respectivos (#24)

--- controler.js
+++ controler.js
@@ -9,5 +9,30 @@ function Cbinary(req, res){
     res.send(cript.binary(req.query.text, req.query.type))
 };
 
+function Ccaesar(req, res){
+    res.json({"test": "test"});
+};
+
+function Cvigenere(req, res){
+    res.json({"test": "test"});
+};
+
+function Cautokey(req, res){
+    res.json({"test": "test"});
+};
+
+function Cpolybius(req, res){
+    res.json({"test": "test"});
+};
+
+function Cnihilist(req, res){
+    res.json({"test": "test"});
+};
+
 exports.test = test;
 exports.Cbinary = Cbinary;
+exports.Ccaesar = Ccaesar;
+exports.Cvigenere = Cvigenere;
+exports.Cautokey = Cautokey;
+exports.Cpolybius = Cpolybius;
+exports.Cnihilist = Cnihilist;
--- rotas.js
+++ rotas.js
@@ -7,4 +7,14 @@ router.get("/test", controler.test);
 
 router.get("/Binary", controler.Cbinary);
 
+router.get("/CaesarCipher", controler.Ccaesar);
+
+router.get("/VigenereCipher", controler.Cvigenere);
+
+router.get("/AutokeyCipher", controler.Cautokey);
+
+router.get("/PolybiusSquare", controler.Cpolybius);
+
+router.get("/NihilistCipher", controler.Cnihilist);
+
 module.exports = router;
