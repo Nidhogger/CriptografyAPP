f98f4ffbd59995f49e36b70daad939cc26d0120b
Nidhogger <62850635+Nidhogger@users.noreply.github.com>
2022-08-27 13:57:21 -0300

    criação de middleware para validação de querry string (#26)
    * criação de middleware para validação de querry string
    
    * Refatoração do Middleware de Validação de Querry String

--- config.js
+++ config.js
@@ -4,6 +4,31 @@ const router = require("./rotas.js");
 
 const app = express();
 
+app.use(function(req,res,next){
+    if(req.path === "/CaesarCipher"){
+        if(req.query.text == "" || req.query.text == null || req.query.text == undefined){
+            res.status(400).send("houve um problema com o texto. ")
+        }
+        if(req.query.direction != "left" && req.query.direction != "right"){
+            res.status(400).send("houve um problema com a direção. ")
+        }
+        if(/\d/g.test(req.query.key)){
+            
+        }else{
+            res.status(400).send("houve um problema com a Key. ")
+        }
+    }
+    if(req.path === "/Binary"){
+        if(req.query.text == "" || req.query.text == null || req.query.text == undefined){
+            res.status(400).send("houve um problema com o texto. ")
+        }
+        if(req.query.type != "Decript" && req.query.type != "Encript"){
+            res.status(400).send("houve um problema com o type. ")
+        }
+    }
+    next();
+});
+
 app.use(bodyp.json());
 
 app.use(bodyp.urlencoded({
