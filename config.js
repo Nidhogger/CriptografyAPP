8d08861dd83c3ede89e3e43f002cec9aa1c1c056
Nidhogger <igorrobertosandri@gmail.com>
2022-08-27 13:12:04 -0300

    criação de middleware para validação de querry string

--- config.js
+++ config.js
@@ -4,6 +4,38 @@ const router = require("./rotas.js");
 
 const app = express();
 
+app.use(function(req,res,next){
+    if(req.path === "/CaesarCipher"){
+        let mensagem = "";
+        if(req.query.text == "" || req.query.text == null || req.query.text == undefined){
+            mensagem += "houve um problema com o texto. ";
+            res.send(mensagem)
+        }
+        if(req.query.direction != "left" && req.query.direction != "right"){
+            mensagem += "houve um problema com a direção. "
+            res.send(mensagem)
+        }
+        if(/\d/g.test(req.query.key)){
+            
+        }else{
+            mensagem += "houve um problema com a Key. "
+            res.send(mensagem)
+        }
+    }
+    if(req.path === "/Binary"){
+        let mensagem = "";
+        if(req.query.text == "" || req.query.text == null || req.query.text == undefined){
+            mensagem += "houve um problema com o texto. ";
+            res.send(mensagem)
+        }
+        if(req.query.type != "Decript" && req.query.type != "Encript"){
+            mensagem += "houve um problema com o type. "
+            res.send(mensagem)
+        }
+    }
+    next();
+});
+
 app.use(bodyp.json());
 
 app.use(bodyp.urlencoded({
