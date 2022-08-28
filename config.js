cc4192b6ea9655b893adee9ded9e6b5167b6d5a7
Nidhogger <igorrobertosandri@gmail.com>
2022-08-28 01:49:52 -0300

    Implementação de Vigenere chipher e metodos relacionados

--- config.js
+++ config.js
@@ -26,6 +26,17 @@ app.use(function(req,res,next){
             res.status(400).send("houve um problema com o type. ")
         }
     }
+    if(req.path === "/VigenereCipher"){
+        if(req.query.text == "" || req.query.text == null || req.query.text == undefined){
+            res.status(400).send("houve um problema com o texto. ")
+        }
+        if(req.query.direction != "left" && req.query.direction != "right"){
+            res.status(400).send("houve um problema com a direção. ")
+        }
+        if(req.query.key == "" || req.query.key == null || req.query.key == undefined){
+            res.status(400).send("houve um problema com a key. ")
+        }
+    }
     next();
 });
 
